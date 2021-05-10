cee62a2f123af416d9cfdb19d9eba27ba9151e43
Oren <halevyoren@gmail.com>
2021-05-10 18:34:40 +0300

    handling validation and mongoose id errors

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -17,6 +17,18 @@ const errorMiddleware = (err, req, res, next) => {
     let error = { ...err };
     error.message = err.message;
 
+    // Wrong mongoose object id error
+    if (err.name === 'CastError') {
+      const message = `Resource not found. Invalid: ${err.path}`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling mongoose validation error
+    if (err.name === 'ValidationError') {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
       success: false,
       message: error.message || 'Internal server error'
