dabf89dfe37fdbcb69eda8504ca45e15258e7233
Oren <halevyoren@gmail.com>
2021-05-11 18:03:40 +0300

    handling errors (duplicate key error and JWT errors)

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -29,6 +29,24 @@ const errorMiddleware = (err, req, res, next) => {
       error = new ErrorHandler(message, 400);
     }
 
+    // Handling mongoose duplicate key error
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling wrong JWT error
+    if (err.name === 'JsonWebTokenError') {
+      const message = 'JSON Web Token is invalid. Please try again.';
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling expired JWT error
+    if (err.name === 'TokenExpiredError') {
+      const message = 'JSON Web Token has expired. Please try again.';
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
       success: false,
       message: error.message || 'Internal server error'
