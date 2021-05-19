ffd2139053153780c44e621bc91439c2100a195a
Oren <halevyoren@gmail.com>
2021-05-19 10:14:30 +0300

    product actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,9 +3,13 @@ import {
   GET_ALL_PRODUCTS_REQUEST,
   GET_ALL_PRODUCTS_SUCCESS,
   GET_ALL_PRODUCTS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
   CLEAR_ERRORS
 } from '../constants/productConstants';
 
+// Get all products
 export const getAllProducts = () => async (dispatch) => {
   try {
     dispatch({
@@ -21,6 +25,27 @@ export const getAllProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: GET_ALL_PRODUCTS_FAIL,
+      payload: error.response.data.message
+    });
+  }
+};
+
+//Get Product details
+export const getProductDetails = (product_id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: PRODUCT_DETAILS_REQUEST
+    });
+
+    const { data } = await axios.get(`/api/products/${product_id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data.product
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload: error.response.data.message
     });
   }
