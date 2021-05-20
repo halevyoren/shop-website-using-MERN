db5b181efaf63fa9fd93b6e230b2a0b0c70443c5
Oren <halevyoren@gmail.com>
2021-05-20 13:31:27 +0300

    added - getProducts returns number of products after filter as well

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -13,15 +13,20 @@ const getProducts = catchAsyncErrors(async (req, res, next) => {
 
   const apiFeatures = new APIFeatures(Product.find(), req.query)
     .search()
-    .filter()
-    .pagination(4);
+    .filter();
 
-  const products = await apiFeatures.query;
+  let products = await apiFeatures.query;
+  const filteredProductsCount = products.length;
+
+  apiFeatures.pagination(resPerPage);
+
+  products = await apiFeatures.query;
 
   res.status(200).json({
     success: true,
     productCount,
     resPerPage,
+    filteredProductsCount,
     products
   });
 });
