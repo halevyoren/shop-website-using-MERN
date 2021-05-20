d441198e8532d333d90e4feecffce1c105b561d8
Oren <halevyoren@gmail.com>
2021-05-20 17:53:08 +0300

    added - login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
 import Home from './components/Home';
 import ProductDetails from './components/product/ProductDetails';
+import Login from './components/user/Login'
 
 import './App.css';
 
@@ -17,6 +18,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/search/:keyword' component={Home} />
           <Route path='/products/:id' exact component={ProductDetails} />
+          <Route path='/login' exact component={Login} />
         </div>
         <Footer />
       </div>
