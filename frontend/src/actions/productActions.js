406b5949df3dac05c2226c29d58e3301b1e3176e
Oren <halevyoren@gmail.com>
2021-06-09 11:04:24 +0300

    added actions for posting review

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -6,42 +6,43 @@ import {
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
+  NEW_REVIEW_REQUEST,
+  NEW_REVIEW_RESET,
+  NEW_REVIEW_SUCCESS,
+  NEW_REVIEW_FAIL,
   CLEAR_ERRORS
 } from '../constants/productConstants';
 
 // Get all products
-export const getAllProducts = (
-  keyword = '',
-  currentPage = 1,
-  priceRange,
-  category,
-  minRating = 0
-) => async (dispatch) => {
-  try {
-    dispatch({
-      type: GET_ALL_PRODUCTS_REQUEST
-    });
-    // if max price is 1000$ then show all prices above 1000$ as well
-    const maxPrice = priceRange[1] < 1000 ? `&price[lte]=${priceRange[1]}` : '';
+export const getAllProducts =
+  (keyword = '', currentPage = 1, priceRange, category, minRating = 0) =>
+  async (dispatch) => {
+    try {
+      dispatch({
+        type: GET_ALL_PRODUCTS_REQUEST
+      });
+      // if max price is 1000$ then show all prices above 1000$ as well
+      const maxPrice =
+        priceRange[1] < 1000 ? `&price[lte]=${priceRange[1]}` : '';
 
-    // if there are chosen categorys the show only them
-    const categoryFilter = category ? `&category=${category}` : '';
+      // if there are chosen categorys the show only them
+      const categoryFilter = category ? `&category=${category}` : '';
 
-    const link = `/api/products?keyword=${keyword}&page=${currentPage}&price[gte]=${priceRange[0]}${maxPrice}${categoryFilter}&ratings[gte]=${minRating}`;
+      const link = `/api/products?keyword=${keyword}&page=${currentPage}&price[gte]=${priceRange[0]}${maxPrice}${categoryFilter}&ratings[gte]=${minRating}`;
 
-    const { data } = await axios.get(link);
+      const { data } = await axios.get(link);
 
-    dispatch({
-      type: GET_ALL_PRODUCTS_SUCCESS,
-      payload: data
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ALL_PRODUCTS_FAIL,
-      payload: error.response.data.message
-    });
-  }
-};
+      dispatch({
+        type: GET_ALL_PRODUCTS_SUCCESS,
+        payload: data
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_ALL_PRODUCTS_FAIL,
+        payload: error.response.data.message
+      });
+    }
+  };
 
 //Get Product details
 export const getProductDetails = (product_id) => async (dispatch) => {
@@ -59,6 +60,33 @@ export const getProductDetails = (product_id) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_DETAILS_FAIL,
+      payload: error.response.data.message
+    });
+  }
+};
+
+//Get Product details
+export const newReview = (reviewData) => async (dispatch) => {
+  try {
+    dispatch({
+      type: NEW_REVIEW_REQUEST
+    });
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const { data } = await axios.put(`/api/review`, reviewData, config);
+
+    dispatch({
+      type: NEW_REVIEW_SUCCESS,
+      payload: data.success
+    });
+  } catch (error) {
+    dispatch({
+      type: NEW_REVIEW_FAIL,
       payload: error.response.data.message
     });
   }
