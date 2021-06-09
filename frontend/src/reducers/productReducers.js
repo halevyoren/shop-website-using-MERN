3d31586302f94a04d1974f40047de406712c1889
Oren <halevyoren@gmail.com>
2021-06-09 11:04:48 +0300

    added reducer for posting review

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -5,6 +5,10 @@ import {
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
+  NEW_REVIEW_REQUEST,
+  NEW_REVIEW_RESET,
+  NEW_REVIEW_SUCCESS,
+  NEW_REVIEW_FAIL,
   CLEAR_ERRORS
 } from '../constants/productConstants';
 
@@ -55,6 +59,39 @@ export const productDetailsReducer = (state = { product: {} }, action) => {
         ...state,
         loading: false,
         product: action.payload
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null
+      };
+    default:
+      return state;
+  }
+};
+
+export const newReviewsReducer = (state = {}, action) => {
+  switch (action.type) {
+    case NEW_REVIEW_REQUEST:
+      return {
+        ...state,
+        loading: true
+      };
+    case NEW_REVIEW_SUCCESS:
+      return {
+        loading: false,
+        success: action.payload
+      };
+    case NEW_REVIEW_FAIL:
+      return {
+        ...state,
+        loading: false,
+        product: action.payload
+      };
+    case NEW_REVIEW_RESET:
+      return {
+        ...state,
+        success: false
       };
     case CLEAR_ERRORS:
       return {
