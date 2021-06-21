a1f62f2525e0ef2fa2ee1d1da715ad17509592b9
Oren <halevyoren@gmail.com>
2021-06-21 14:48:49 +0300

    added reducer for deleting product

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -9,6 +9,10 @@ import {
   NEW_PRODUCT_RESET,
   NEW_PRODUCT_SUCCESS,
   NEW_PRODUCT_FAIL,
+  DELETE_PRODUCT_REQUEST,
+  DELETE_PRODUCT_RESET,
+  DELETE_PRODUCT_SUCCESS,
+  DELETE_PRODUCT_FAIL,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
@@ -19,7 +23,7 @@ import {
   CLEAR_ERRORS
 } from '../constants/productConstants';
 
-export const productReducer = (state = { products: [] }, action) => {
+export const productsReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case GET_ALL_PRODUCTS_REQUEST:
     case ADMIN_PRODUCTS_REQUEST:
@@ -79,6 +83,40 @@ export const newProductReducer = (state = {product: {}}, action) => {
       return {
         ...state,
         success: false
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null
+      };
+    default:
+      return state;
+  }
+};
+
+// admin - for deleteting and updating a product
+export const productReducer = (state = {}, action) => {
+  switch (action.type) {
+    case DELETE_PRODUCT_REQUEST:
+      return {
+        ...state,
+        loading: true
+      };
+    case DELETE_PRODUCT_SUCCESS:
+      return {
+        loading: false,
+        isDeleted: action.payload
+      };
+    case DELETE_PRODUCT_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+    case DELETE_PRODUCT_RESET:
+      return {
+        ...state,
+        isDeleted: false
       };
     case CLEAR_ERRORS:
       return {
