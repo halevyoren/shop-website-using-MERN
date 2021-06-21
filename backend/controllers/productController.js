641e95d8f87a21a3b3d94352a41a2932e168a4ba
Oren <halevyoren@gmail.com>
2021-06-21 12:11:43 +0300

    added newProduct option for uploading multiple images

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const Product = require('../models/product');
 const ErrorHandler = require('../utils/errorHandler');
 const APIFeatures = require('../utils/apiFeatures');
+const cloudinary = require('cloudinary');
 
 // @route   GET api/products
 // @desc    Getting all products
@@ -63,6 +64,28 @@ const getProductById = catchAsyncErrors(async (req, res, next) => {
 // @desc    Creating new product
 // @access  Private
 const newProduct = catchAsyncErrors(async (req, res, next) => {
+  let images = [];
+  // if only one image is uploaded then it will be received as a string else it will be an array
+  if (typeof req.body.images === 'string') {
+    images.push(req.body.images);
+  } else {
+    images = req.body.images;
+  }
+
+  let imagesLinks = [];
+  
+  for (let i = 0; i < images.length; i++) {
+    const result = await cloudinary.v2.uploader.upload(images[i], {
+        folder: 'shop/products'
+    });
+
+    imagesLinks.push({
+        public_id: result.public_id,
+        url: result.secure_url
+    })
+}
+
+  req.body.images = imagesLinks;
   req.body.user = req.user.id;
 
   const product = await Product.create(req.body);
