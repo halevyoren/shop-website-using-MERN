c311eef4f0bc3e10721ae3df444d12067d141406
Oren <halevyoren@gmail.com>
2021-06-21 15:57:28 +0300

    added actions to update product

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -9,6 +9,12 @@ import {
   NEW_PRODUCT_REQUEST,
   NEW_PRODUCT_SUCCESS,
   NEW_PRODUCT_FAIL,
+  UPDATE_PRODUCT_REQUEST,
+  UPDATE_PRODUCT_SUCCESS,
+  UPDATE_PRODUCT_FAIL,
+  DELETE_PRODUCT_REQUEST,
+  DELETE_PRODUCT_SUCCESS,
+  DELETE_PRODUCT_FAIL,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
@@ -49,7 +55,7 @@ export const getAllProducts =
     }
   };
 
-//Get Product details
+// Creat new Product - admin
 export const newProduct = (productData) => async (dispatch) => {
   try {
     dispatch({
@@ -75,6 +81,58 @@ export const newProduct = (productData) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: NEW_PRODUCT_FAIL,
+      payload: error.response.data.message
+    });
+  }
+};
+
+// Update Product - admin
+export const updateProduct = (product_id, productData) => async (dispatch) => {
+  try {
+    dispatch({
+      type: UPDATE_PRODUCT_REQUEST
+    });
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const { data } = await axios.put(
+      `/api/products/admin/${product_id}`,
+      productData,
+      config
+    );
+
+    dispatch({
+      type: UPDATE_PRODUCT_SUCCESS,
+      payload: data.success
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PRODUCT_FAIL,
+      payload: error.response.data.message
+    });
+  }
+};
+
+// Delete Product - admin
+export const deleteProduct = (product_id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: DELETE_PRODUCT_REQUEST
+    });
+
+    const { data } = await axios.delete(`/api/products/admin/${product_id}`);
+
+    dispatch({
+      type: DELETE_PRODUCT_SUCCESS,
+      payload: data.success
+    });
+  } catch (error) {
+    dispatch({
+      type: DELETE_PRODUCT_FAIL,
       payload: error.response.data.message
     });
   }
