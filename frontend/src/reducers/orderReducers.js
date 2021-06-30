2698ec45377ac64fa92ce749fb3f7e808070eca7
Oren <halevyoren@gmail.com>
2021-06-30 13:16:28 +0300

    added reducerss for updating order status

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -8,6 +8,10 @@ import {
   ALL_ORDERS_REQUEST,
   ALL_ORDERS_SUCCESS,
   ALL_ORDERS_FAIL,
+  UPDATE_ORDER_REQUEST,
+  UPDATE_ORDER_SUCCESS,
+  UPDATE_ORDER_RESET,
+  UPDATE_ORDER_FAIL,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
@@ -131,6 +135,41 @@ export const allOrdersReducer = (state = { orders: [] }, action) => {
         error: null
       };
 
+    default:
+      return state;
+  }
+};
+
+// admin - for updating a order
+export const orderReducer = (state = {}, action) => {
+  switch (action.type) {
+    case UPDATE_ORDER_REQUEST:
+      return {
+        ...state,
+        loading: true
+      };
+    case UPDATE_ORDER_SUCCESS:
+      return {
+        loading: false,
+        isUpdated: action.payload
+      };
+
+    case UPDATE_ORDER_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+    case UPDATE_ORDER_RESET:
+      return {
+        ...state,
+        isUpdated: false
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null
+      };
     default:
       return state;
   }
